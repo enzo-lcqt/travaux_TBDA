ef9ed7a1c1cbad801d017d1febcc4e3d130d8c08
locquet <enzolocquet@gmail.com>
2023-04-25 16:27:10 +0200

    commencer exo6

---
+++ java/exo_6/script.js
@@ -0,0 +1,27 @@
+var bouton1= document.getElementById("Id_btn1");
+bouton1.addEventListener("click",clickbtn1);
+
+function clickbtn1(){
+
+}
+
+var bouton2= document.getElementById("Id_btn2");
+bouton2.addEventListener("click",clickbtn2);
+
+function clickbtn2(){
+
+}
+
+var bouton3= document.getElementById("Id_btn3");
+bouton3.addEventListener("click",clickbtn3);
+
+function clickbtn3(){
+
+}
+
+var bouton4= document.getElementById("Id_btn4");
+bouton4.addEventListener("click",clickbtn4);
+
+function clickbtn4(){
+
+}
