3e6bbb31d13929ffc7bef9dd83eee288098d46a8
locquet <enzolocquet@gmail.com>
2023-04-27 14:32:32 +0200

    jarditou

--- java/exo_6/script.js
+++ java/exo_6/script.js
@@ -5,3 +5,12 @@ function clickbtn1(){
     var a="100"
     alert("Ceci est une chaine de caractères: "+a);
 }
+
+var bouton2= document.getElementById("Id_btn2");
+bouton2.addEventListener("click",clickbtn2);
+
+function clickbtn2(){
+    var b="100"
+    b=x--
+    alert("operateur de décrementation : "+b);
+}
---
+++ java/test write/script.js
@@ -0,0 +1,14 @@
+let nombre1 = 100;
+let nombre2 = 100;
+let nombre3 = 1.00;
+let nombre4 = x++;
+
+nombre1 = 100;
+nombre2 = 
+nombre3 = 
+nombre4 =
+
+console.log(nomber1);
+console.log(nomber2);
+console.log(nomber3);
+console.log(nomber4);
