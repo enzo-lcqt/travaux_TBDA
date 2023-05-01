36a7eed3d765eac42bb3437f4ec085aeb57ddf60
enzo-lcqt <enzolocquet@gmail.com>
2023-05-01 20:08:22 +0200

    debut exo8

---
+++ java/exo_8/script.js
@@ -0,0 +1,15 @@
+// exo1
+var prenom = "prenom";
+i = 1
+while(prenom != "")
+{
+    prenom=prompt("Entrez le prenom N°"+i);
+    i++;
+    console.log(prenom +i)
+    if(prenom==null){
+        alert("Vous avez cliqué sur Annuler");
+        break;
+        }
+}
+// exo2
+
