91483c8511c9d7b91c54e33cf44bb048b8c76f6a
locquet <enzolocquet@gmail.com>
2023-05-03 16:58:22 +0200

    debut9

--- java/test_fonction/script.js
+++ java/test_fonction/script.js
@@ -1,9 +1,22 @@
-// exo1
 var cube = prompt("ecrire un nombre");
 var calc = prompt("ecrire un multiplicateur");
 resultat1 = Number(cube) * Number(cube) * Number(cube)
 resultat2 = Number(cube) * Number(calc);
-
-
 document.getElementById("cube").innerHTML = ("Le cube de "+cube+" est égal à : "+resultat1);
 document.getElementById("calcule").innerHTML = ("le produit de "+cube+" x "+calc+" est égal à : "+resultat2);
+
+function produit(cube, calc){
+    
+}
+
+
+
+function afficheImg(img){
+var imagejavascript = document.createElement("img");
+imagejavascript.src = "/exo_9/img/papillon.jpg";
+document.body.appendChild(imagejavascript)
+}
+afficheImg("/exo_9/img/papillon.jpg")
+
+
+
