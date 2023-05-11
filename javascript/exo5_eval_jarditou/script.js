edb2114131be23e2221fa003f57548264ecade3b
locquet <enzolocquet@gmail.com>
2023-05-11 09:28:50 +0200

    eval_fini

--- javascript/exo5_eval_jarditou/script.js
+++ javascript/exo5_eval_jarditou/script.js
@@ -1,4 +1,3 @@
-function validateForm(){
 let nom = document.getElementById("nom").value.trim()
 let prenom  = document.getElementById("prenom").value.trim()
 let date = document.getElementById("date").value.trim()
@@ -19,41 +18,35 @@ let demande_error = document.getElementById("demande_error");
 let question_error = document.getElementById("question_error");
 
 let valid = true;
+
+function validateForm(){
+
 // nom
 if (nom === ""){
-    nom_error.innerHTML = "veuillez entrez votre nom"
+    nom_error.innerHTML = "veuillez entrez votre nom";
+    nom_error.style.color="red";
     valid = false;
-}else {
-    nom_error.innerHTML = "";
 }
 // prenom
 if (prenom === ""){
     prenom_error.innerHTML ="veuillez entrez votre nom"
-    valid = False
-}else {
-    prenom_error.innerHTML = "";
+    valid = false;
 }
 // date
 if (date === ""){
     date_error.innerHTML = "veuillez entre une date"
     valid = false;
-}else {
-    date_error.innerHTML = "";
 }
 // adresse
 if (adresse === ""){
     adresse_error.innerHTML = "veuillez entrez votre adresse"
     valid = false;
-}else {
-    adresse_error.innerHTML = "";
 }
 
 // ville
 if(ville === ""){
     ville_error.innerHTML = "veuillez entrez votre ville"
     valid = false;
-}else {
-    ville_error.innerHTML= "";
 }
 
 // mail
@@ -63,24 +56,19 @@ if(mail === ""){
 } else if (!/\S+@\S+\.\S+/.test(mail)){
     mail_error.innerHTML = "veuillez entrez un mail valide";
     valid = false;
-}else {
-    mail_error.innerHTML = "";
 }
 
 // demande
 if (demande === ""){
     demande_error.innerHTML = "veuillez entrez votre demande"
     valid = false;
-}else {
-    demande_error.innerHTML = ""
 }
+
 
 // question
 if (question === ""){
     question_error.innerHTML = "veuillez poser votre question"
     valid = false;
-}else {
-    question_error.innerHTML = ""
 }
 
 //  code postal
@@ -90,11 +78,10 @@ if (code === ""){
 }else if(!/^[0-9]{5}$/.test(code)){
     code_error.innerHTML = "le code doit être de 5 chiffres"
     valid = false;
-}else {
-    code_error.innerHTML = "";
 }
 
 if (valid){
     document.myForm.submit();
 }
 }
+validateForm();
