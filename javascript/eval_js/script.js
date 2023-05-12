69923b88faf10d0cc38470c276b034dcb5b7c65a
locquet <enzolocquet@gmail.com>
2023-05-12 11:37:53 +0200

    exo3

--- javascript/eval_js/script.js
+++ javascript/eval_js/script.js
@@ -38,26 +38,26 @@ multiplication(n)
 let box;
 let prenom = prompt("Veuillez saisir un prenom");
 
-const tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane" ,"<br>"];
+const l = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane" ,"<br>"];
  
 
-box = tab.indexOf(prenom);
+box = l.indexOf(prenom);
  
 
 document.write(prenom+"<br>");
 while(box != -1){
 
-    tab.splice(box,1);
+    l.splice(box,1);
     prenom = prompt("Veuillez saisir un prenom");
-    box = tab.indexOf(prenom);
-    tab.splice(box,1);
+    box = l.indexOf(prenom);
+    l.splice(box,1);
     document.write("Hop un prénom de trouver<br>"+prenom+"<br>");
 
 
 
 } 
     document.write("Ce prénom ne se trouve pas dans la liste <br>");
-    document.write("Voici la liste finale des prénoms restants a trouver : " +l);
+    document.write("Voici la liste finale des prénoms restants a trouver : " +l+"<br>");
 
 
 //exo4
